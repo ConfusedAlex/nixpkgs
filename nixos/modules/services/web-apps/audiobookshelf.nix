df119b84e20b91c907a1a23606b41efc7e0709e2
confusedalex <alex@confusedalex.dev>
2026-03-28 13:47:32 +0100

    audiobookshelf: make dataDir an absolute path

--- nixos/modules/services/web-apps/audiobookshelf.nix
+++ nixos/modules/services/web-apps/audiobookshelf.nix
@@ -18,8 +18,8 @@ in
       package = mkPackageOption pkgs "audiobookshelf" { };
 
       dataDir = mkOption {
-        description = "Path to Audiobookshelf config and metadata inside of /var/lib.";
-        default = "audiobookshelf";
+        description = "Default data directory for Audiobookshelf.";
+        default = "/var/lib/audiobookshelf";
         type = types.str;
       };
 
@@ -67,8 +67,8 @@ in
         Type = "simple";
         User = cfg.user;
         Group = cfg.group;
-        StateDirectory = cfg.dataDir;
-        WorkingDirectory = "/var/lib/${cfg.dataDir}";
+        StateDirectory = "audiobookshelf";
+        WorkingDirectory = cfg.dataDir;
         ExecStart = "${cfg.package}/bin/audiobookshelf --host ${cfg.host} --port ${toString cfg.port}";
         Restart = "on-failure";
       };
